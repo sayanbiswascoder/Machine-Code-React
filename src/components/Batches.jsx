414d2bcac19e01bcecd5e23444f53cd2039fff6e
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-07-25 14:29:59 +0530

    updating courses for batches page

--- src/components/Batches.jsx
+++ src/components/Batches.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Header from './Header'
 import ChaiCode from './ChaiCode.jsx'
 import courses from './courses.js'
@@ -8,6 +8,17 @@ const Batches = () => {
   const [coursePerPage, setCoursePerPage] = useState(3)
   const [currentPage, setCurrentPage] = useState(0)
   const [search, setSearch] = useState([false, ""])
+
+  useEffect(() => {
+    document.addEventListener('keydown', (e)=> {
+      if(e.metaKey || e.altKey){
+        if(e.key == "k"){
+          document.getElementById("search").focus()
+        }
+      }
+    })
+  }, [])
+  
 
   const nextPage = () => {
     if(currentPage + 1 == (Number.parseInt(courses.length / coursePerPage) + ((courses.length % coursePerPage) != 0 ? 1 : 0)))return;
@@ -27,12 +38,12 @@ const Batches = () => {
           <h2 className='text-xl font-bold'>Batches</h2>
           <p className='text-gray-400 mb-2'>Create learner’s batch and share information at the same time.</p>
 
-          <div className='flex gap-2 mt-4'>
-            <input value={search[1]} onChange={e=> setSearch([false, e.target.value])} placeholder='Search by Title (alt+k or cmd+k)' className="rounded outline-none border p-1 border-solid" type="text" name="search" />
+          <div className='flex gap-2 mt-4 w-full'>
+            <input id='search' value={search[1]} onChange={e=> setSearch([false, e.target.value])} placeholder='Search by Title (alt+k or cmd+k)' className="rounded outline-none border p-1 border-solid w-[40%]" type="text" name="search" />
             <button onClick={()=> setSearch([true, search[1]])} className='rounded h-full bg-[#6C6BAF] p-1 px-2 text-white'>Search</button>
           </div>
           <div className='w-full flex flex-col items-end'>
-            <div className=' max-h-[50vh] overflow-scroll mt-6 rounded-lg border border-solid border-[#7D7D7D]'>
+            <div className='w-full max-h-[50vh] overflow-scroll mt-6 rounded-lg border border-solid border-[#7D7D7D]'>
               <table className='w-full'>
                 <thead className='bg-[#F2F2F2] sticky top-0 border-b border-solid border-[#7D7D7D]'>
                   <tr>
@@ -90,11 +101,11 @@ const Batches = () => {
                 <option value={5} >5</option>
                 <option value={10} >10</option>
               </select>
-              <svg onClick={previousPage} width="8" height="15" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg onClick={previousPage} className='cursor-pointer' width="8" height="15" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className="socIcon" d="M16.3426 26.1497C16.7637 26.574 17 27.147 17 27.7441C17 28.3412 16.7637 28.9142 16.3426 29.3385C16.1358 29.548 15.8892 29.7144 15.6173 29.828C15.3455 29.9415 15.0537 30 14.7589 30C14.4642 30 14.1724 29.9415 13.9005 29.828C13.6286 29.7144 13.3821 29.548 13.1753 29.3385L0.656395 16.5961C0.235807 16.1708 0 15.5974 0 15C0 14.4026 0.235807 13.8292 0.656395 13.4039L13.1753 0.661457C13.3821 0.451963 13.6286 0.285596 13.9005 0.172038C14.1724 0.0584801 14.4642 0 14.7589 0C15.0537 0 15.3455 0.0584801 15.6173 0.172038C15.8892 0.285596 16.1358 0.451963 16.3426 0.661457C16.7637 1.08577 17 1.65877 17 2.2559C17 2.85302 16.7637 3.42602 16.3426 3.85034L6.07579 15.0049L16.3426 26.1497Z" fill={currentPage == 0 ? "#D6D6D6" : "black"} />
               </svg>
               <span>{currentPage + 1}</span>
-              <svg onClick={nextPage} className='rotate-180' width="8" height="15" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg onClick={nextPage} className='rotate-180 cursor-pointer' width="8" height="15" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className="socIcon" d="M16.3426 26.1497C16.7637 26.574 17 27.147 17 27.7441C17 28.3412 16.7637 28.9142 16.3426 29.3385C16.1358 29.548 15.8892 29.7144 15.6173 29.828C15.3455 29.9415 15.0537 30 14.7589 30C14.4642 30 14.1724 29.9415 13.9005 29.828C13.6286 29.7144 13.3821 29.548 13.1753 29.3385L0.656395 16.5961C0.235807 16.1708 0 15.5974 0 15C0 14.4026 0.235807 13.8292 0.656395 13.4039L13.1753 0.661457C13.3821 0.451963 13.6286 0.285596 13.9005 0.172038C14.1724 0.0584801 14.4642 0 14.7589 0C15.0537 0 15.3455 0.0584801 15.6173 0.172038C15.8892 0.285596 16.1358 0.451963 16.3426 0.661457C16.7637 1.08577 17 1.65877 17 2.2559C17 2.85302 16.7637 3.42602 16.3426 3.85034L6.07579 15.0049L16.3426 26.1497Z" fill={currentPage + 1 == (Number.parseInt(courses.length / coursePerPage) + ((courses.length % coursePerPage) != 0 ? 1 : 0)) ? "#D6D6D6" : "black"} />
               </svg>
             </div>
--- src/components/courses.js
+++ src/components/courses.js
@@ -1,6 +1,10 @@
 import SQLcover from "../assets/SQL-cover.png"
 import JavascriptCover from "../assets/30 days javascript cover.png"
 import InterviewCover from "../assets/interview preparation cover.png"
+import GitCover from "../assets/git cover.jpg"
+import ReactCover from "../assets/react cover.jpg"
+import ReactNativeCover from "../assets/react native cover.jpg"
+import TypescriptCover from "../assets/typescript cover.jpg"
 
 const courses =  [
     {
@@ -31,57 +35,39 @@ const courses =  [
         "status": "Published"
     },
     {
-        "title": "SQL Basics To Advanced Mastery Course",
-        "cover": SQLcover,
-        "start": "20 Jul 2024",
-        "end": "28 Jul 2024",
+        "title": "Git for beginner",
+        "cover": GitCover,
+        "start": "27 Mar 2024",
+        "end": "06 Apr 2024",
         "price": 0,
-        "validity": 180,
+        "validity": 11,
         "status": "Published"
     },
     {
-        "title": "30 Days Of Javascript Challenge",
-        "cover": JavascriptCover,
-        "start": "13 Jul 2024",
-        "end": "12 Aug 2024",
+        "title": "Compleete React Course for Beginner",
+        "cover": ReactCover,
+        "start": "04 Sep 2023",
+        "end": "08 Dec 2023",
         "price": 0,
-        "validity": 33,
-        "status": "Unpublished"
-    },
-    {
-        "title": "Interview Preparation With Javascript 2.0",
-        "cover": InterviewCover,
-        "start": "02 Aug 2024",
-        "end": "15 Sep 2024",
-        "price": 10000,
-        "validity": 365,
+        "validity": 66,
         "status": "Published"
     },
     {
-        "title": "SQL Basics To Advanced Mastery Course",
-        "cover": SQLcover,
-        "start": "20 Jul 2024",
-        "end": "28 Jul 2024",
+        "title": "React Native Masttery",
+        "cover": ReactNativeCover,
+        "start": "26 Jan 2023",
+        "end": "11 Jun 2023",
         "price": 0,
-        "validity": 180,
+        "validity": 140,
         "status": "Published"
     },
     {
-        "title": "30 Days Of Javascript Challenge",
-        "cover": JavascriptCover,
-        "start": "13 Jul 2024",
-        "end": "12 Aug 2024",
+        "title": "Typescript course",
+        "cover": TypescriptCover,
+        "start": "07 Sep 2022",
+        "end": "27 Oct 2022",
         "price": 0,
-        "validity": 33,
-        "status": "Unpublished"
-    },
-    {
-        "title": "Interview Preparation With Javascript 2.0",
-        "cover": InterviewCover,
-        "start": "02 Aug 2024",
-        "end": "15 Sep 2024",
-        "price": 10000,
-        "validity": 365,
+        "validity": 51,
         "status": "Published"
     },
     {
@@ -112,57 +98,39 @@ const courses =  [
         "status": "Published"
     },
     {
-        "title": "SQL Basics To Advanced Mastery Course",
-        "cover": SQLcover,
-        "start": "20 Jul 2024",
-        "end": "28 Jul 2024",
+        "title": "Git for beginner",
+        "cover": GitCover,
+        "start": "27 Mar 2024",
+        "end": "06 Apr 2024",
         "price": 0,
-        "validity": 180,
+        "validity": 11,
         "status": "Published"
     },
     {
-        "title": "30 Days Of Javascript Challenge",
-        "cover": JavascriptCover,
-        "start": "13 Jul 2024",
-        "end": "12 Aug 2024",
+        "title": "Compleete React Course for Beginner",
+        "cover": ReactCover,
+        "start": "04 Sep 2023",
+        "end": "08 Dec 2023",
         "price": 0,
-        "validity": 33,
-        "status": "Unpublished"
-    },
-    {
-        "title": "Interview Preparation With Javascript 2.0",
-        "cover": InterviewCover,
-        "start": "02 Aug 2024",
-        "end": "15 Sep 2024",
-        "price": 10000,
-        "validity": 365,
+        "validity": 66,
         "status": "Published"
     },
     {
-        "title": "SQL Basics To Advanced Mastery Course",
-        "cover": SQLcover,
-        "start": "20 Jul 2024",
-        "end": "28 Jul 2024",
+        "title": "React Native Masttery",
+        "cover": ReactNativeCover,
+        "start": "26 Jan 2023",
+        "end": "11 Jun 2023",
         "price": 0,
-        "validity": 180,
+        "validity": 140,
         "status": "Published"
     },
     {
-        "title": "30 Days Of Javascript Challenge",
-        "cover": JavascriptCover,
-        "start": "13 Jul 2024",
-        "end": "12 Aug 2024",
+        "title": "Typescript course",
+        "cover": TypescriptCover,
+        "start": "07 Sep 2022",
+        "end": "27 Oct 2022",
         "price": 0,
-        "validity": 33,
-        "status": "Unpublished"
-    },
-    {
-        "title": "Interview Preparation With Javascript 2.0",
-        "cover": InterviewCover,
-        "start": "02 Aug 2024",
-        "end": "15 Sep 2024",
-        "price": 10000,
-        "validity": 365,
+        "validity": 51,
         "status": "Published"
     },
     {
@@ -190,6 +158,42 @@ const courses =  [
         "end": "15 Sep 2024",
         "price": 10000,
         "validity": 365,
+        "status": "Published"
+    },
+    {
+        "title": "Git for beginner",
+        "cover": GitCover,
+        "start": "27 Mar 2024",
+        "end": "06 Apr 2024",
+        "price": 0,
+        "validity": 11,
+        "status": "Published"
+    },
+    {
+        "title": "Compleete React Course for Beginner",
+        "cover": ReactCover,
+        "start": "04 Sep 2023",
+        "end": "08 Dec 2023",
+        "price": 0,
+        "validity": 66,
+        "status": "Published"
+    },
+    {
+        "title": "React Native Masttery",
+        "cover": ReactNativeCover,
+        "start": "26 Jan 2023",
+        "end": "11 Jun 2023",
+        "price": 0,
+        "validity": 140,
+        "status": "Published"
+    },
+    {
+        "title": "Typescript course",
+        "cover": TypescriptCover,
+        "start": "07 Sep 2022",
+        "end": "27 Oct 2022",
+        "price": 0,
+        "validity": 51,
         "status": "Published"
     },
 ]
